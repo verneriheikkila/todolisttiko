f42f8563a1eab637c312d027d5b2d04fce4aa5ff
verneriheikkila <verneri.heikkila@gmail.com>
2022-10-11 18:54:05 +0300

    "Ag-grid" tehtävä valmis

--- src/components/Todolist.js
+++ src/components/Todolist.js
@@ -1,9 +1,36 @@
-import React, { useState } from 'react';
-import TodoTable from './TodoTable';
+import React, { useState, useRef } from 'react';
+// import TodoTable from './TodoTable';
+import { AgGridReact } from 'ag-grid-react';
+
+import 'ag-grid-community/styles/ag-grid.css';
+import 'ag-grid-community/styles/ag-theme-material.css';
 
 function Todolist() {
-    const [todo, setTodo] = useState({ description: '', date: '' });
+    const [todo, setTodo] = useState({
+        description: '',
+        date: '',
+        priority: '',
+    });
     const [todos, setTodos] = useState([]);
+    const gridRef = useRef();
+
+    const [columnDefs] = useState([
+        {
+            field: 'description',
+            sortable: true,
+            filter: true,
+            floatingFilter: true,
+        },
+        { field: 'date', sortable: true, filter: true, floatingFilter: true },
+        {
+            field: 'priority',
+            sortable: true,
+            filter: true,
+            floatingFilter: true,
+            cellStyle: (params) =>
+                params.value === 'High' ? { color: 'red' } : { color: 'black' },
+        },
+    ]);
 
     const addTodo = (event) => {
         event.preventDefault();
@@ -15,28 +42,56 @@ function Todolist() {
     };
 
     const deleteTodo = (index) => {
-        setTodos(todos.filter((todo, i) => i !== index));
+        if (gridRef.current.getSelectedNodes().length > 0) {
+            setTodos(
+                todos.filter(
+                    (todo, index) =>
+                        index !==
+                        gridRef.current.getSelectedNodes()[0].childIndex
+                )
+            );
+        } else {
+            alert('No row selected!');
+        }
     };
 
     return (
-        <div className="App">
+        <div>
             <h1>TODO list</h1>
-            <form onSubmit={addTodo}>
-                <input
-                    type="date"
-                    name="date"
-                    value={todo.date}
-                    onChange={inputChanged}
+
+            <input
+                type="text"
+                name="description"
+                value={todo.description}
+                onChange={inputChanged}
+            />
+            <input
+                type="date"
+                name="date"
+                value={todo.date}
+                onChange={inputChanged}
+            />
+            <input
+                type="text"
+                name="priority"
+                value={todo.priority}
+                onChange={inputChanged}
+            />
+            <input type="button" value="Add" onClick={addTodo}></input>
+            <input type="button" value="delete" onClick={deleteTodo}></input>
+            <div
+                className="ag-theme-material"
+                style={{ margin: 'auto', width: '60%', height: 600 }}
+            >
+                <AgGridReact
+                    ref={gridRef}
+                    onGridReady={(params) => (gridRef.current = params.api)}
+                    rowSelection="single"
+                    rowData={todos}
+                    columnDefs={columnDefs}
+                    animateRows={true}
                 />
-                <input
-                    type="desription"
-                    name="description"
-                    value={todo.description}
-                    onChange={inputChanged}
-                />
-                <input type="submit" value="Add" />
-            </form>
-            <TodoTable todos={todos} delete={deleteTodo} />
+            </div>
         </div>
     );
 }
