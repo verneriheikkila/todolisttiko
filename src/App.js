e03ed15ed0c6ae521dbd0879bd63caa84761664c
verneriheikkila <verneri.heikkila@gmail.com>
2022-10-26 23:31:00 +0300

    tehtävä "Tab menu" valmis

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
 import './App.css';
 import Todolist from './components/Todolist';
+import Home from './components/Home';
+import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
+import { Tab, Tabs } from '@mui/material';
+import React from 'react';
 
 function App() {
+    const [value, setValue] = React.useState(0);
+    const handleChange = (event, newValue) => {
+        setValue(newValue);
+    };
     return (
         <div className="App">
-            <Todolist />
+            <BrowserRouter>
+                <Tabs
+                    value={value}
+                    onChange={handleChange}
+                    aria-label="basic tabs example"
+                >
+                    <Tab component={Link} to="/" label="Home" />
+                    <Tab component={Link} to="/todos" label="Todo" />
+                </Tabs>
+                <Routes>
+                    <Route exact path="/" element={<Home />} />
+                    <Route exact path="/todos" element={<Todolist />} />
+                </Routes>
+            </BrowserRouter>
         </div>
     );
 }
---
+++ src/components/Home.js
@@ -0,0 +1,6 @@
+import React from 'react';
+
+function Contact() {
+    return <h2>This is the home page</h2>;
+}
+export default Contact;
